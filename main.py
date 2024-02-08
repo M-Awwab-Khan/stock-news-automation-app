c24be8f8c56a9d9eb1c9f2585cbe5d1dcba27d6b
Awwab <mawwabkhank2006@gmail.com>
2024-02-08 10:24:16 +0500

    feedback on perc_change between two days

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 ALPHAVANTAGE_API_KEY = 'GGBOXIZNYRXWEMKP'
 ALPHAVANTAGE_END_POINT = 'https://www.alphavantage.co/query'
+NEWS_API_KEY = 'c892c61b7eff4d70b2335d9de845ec1d'
+NEWS_API_ENDPOINT = 'https://newsapi.org/v2/everything'
 
-params = {
+alphavantage_params = {
     'function': 'TIME_SERIES_DAILY',
     'symbol': 'TSLA',
     'apikey': 'GGBOXIZNYRXWEMKP'
 }
 
-response = requests.get(ALPHAVANTAGE_END_POINT, params=params)
+response = requests.get(ALPHAVANTAGE_END_POINT, params=alphavantage_params)
 response.raise_for_status()
 
 today = dt.datetime.now()
@@ -26,6 +28,12 @@ db_yesterday_close = float(data["Time Series (Daily)"][db_yesterday_str]["4. clo
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+perc_change = ((yesterday_close - db_yesterday_close) / db_yesterday_close) * 100
+if abs(perc_change) >= 5:
+    print('get news')
+else:
+    print('dont get news')
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
